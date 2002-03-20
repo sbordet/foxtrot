eee8c4eff000e1092b58e8cb89a0bd6bc4570520
Simone Bordet <simone.bordet@gmail.com>
2002-03-20 11:22:00 +0000

    More docs

---
+++ website/htdocs/docs/introduction.php
@@ -0,0 +1,27 @@
+<?php include 'header.php';?>
+
+<tr><td align="center" colspan="3">
+<a href="http://foxtrot.sourceforge.net"><img class="nav-btn-en" hspace="10" src="../images/cnvhome.gif"/></a>
+<a href="toc.php"><img class="nav-btn-en" hspace="10" src="../images/cnvup.gif"/></a>
+<img class="nav-btn-dis" hspace="10" src="../images/cnvprev.gif"/>
+<a href="license.php"><img class="nav-btn-en" hspace="10" src="../images/cnvnext.gif"/></a>
+</td></tr>
+
+<tr><td class="date" colspan="3">Last Updated: $Date$</td></tr>
+
+<tr><td class="documentation">
+
+<h2>Introduction</h2>
+
+<p>Foxtrot is a small but powerful framework for using threads with the Java<sup><font="-2">TM</font></sup>
+Foundation Classes (JFC/Swing).</p>
+<p>It is compatible with the J2SE<sup><font="-2">TM</font></sup>, but can be adapted to work with the
+Java<sup><font="-2">TM</font></sup> Development Kit (JDK) version 1.1.x with a small modification to an import
+statement.<br>
+This is due to the fact that class ActiveEvent moved from package java.awt.peer in JDK 1.1.x to package
+java.awt in J2SE.</p>
+<p>The Foxtrot framework is released under the BSD license.</p>
+
+</td></tr>
+
+<?php include 'footer.php';?>
