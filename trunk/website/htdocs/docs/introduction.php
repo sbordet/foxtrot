c4c6f067fa4d82917b13b1ea1017fb98076bf2ca
biorn_steedom <biorn_steedom@d2b52d26-9d10-0410-9a7c-e9c4ec8e0f90>
2002-04-05 13:12:40 +0000

    Small TM changes

--- trunk/website/htdocs/docs/introduction.php
+++ trunk/website/htdocs/docs/introduction.php
@@ -18,9 +18,9 @@ Foundation Classes (JFC/Swing).</p>
 <p>It is compatible with the J2SE<sup><font="-2">TM</font></sup>, and has been tested with version 1.3.x and 1.4.x.</p>
 <p>The jar containing the Foxtrot core classes (namely foxtrot.jar), from Foxtrot version 1.1, must be included in
 the boot classpath.<br>
-This is due to the fact that in the Java Development Kit (JDK<sup><font="-2">TM</font></sup>), version 1.4.x, the
-mechanism of event dispatching has changed; consequently, for Foxtrot to be compatible with JDK 1.4.x and 1.3.x, the
-Foxtrot implementation is required to be in the boot classpath.</p>
+This is due to the fact that in the Java<sup><font="-2">TM</font></sup> Development Kit (JDK<sup><font="-2">TM</font></sup>),
+version 1.4.x, the mechanism of event dispatching has changed; consequently, for Foxtrot to be compatible with JDK 1.4.x
+and 1.3.x, the Foxtrot implementation is required to be in the boot classpath.</p>
 <p>Below you can find an example of how to set the foxtrot core classes in the boot classpath.</p>
 <p>Let's suppose that your Swing application is contained in my-swing.jar, that the main class is called my.swing.Application,
 and the Foxtrot core classes in foxtrot.jar; then you should start your application with a command line similar to this one:</p>
