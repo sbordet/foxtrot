dc6641a4b6751e6b6dcbd087beef6caa87a855ec
Simone Bordet <simone.bordet@gmail.com>
2002-04-07 10:42:44 +0000

    Import clean up

--- foxtrot/src/examples/foxtrot/examples/SimpleExample.java
+++ foxtrot/src/examples/foxtrot/examples/SimpleExample.java
@@ -8,18 +8,18 @@
 
 package foxtrot.examples;
 
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
 import java.awt.Container;
-import java.awt.GridBagLayout;
 import java.awt.Dimension;
+import java.awt.GridBagLayout;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
 import javax.swing.JButton;
+import javax.swing.JFrame;
 
-import foxtrot.Worker;
 import foxtrot.Task;
+import foxtrot.Worker;
 
 /**
  * A very simple example that shows Swing limits. <br>
