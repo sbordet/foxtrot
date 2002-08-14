a1bafc8a015437889a7e40c0de1f97861368de7b
biorn_steedom <biorn_steedom@d2b52d26-9d10-0410-9a7c-e9c4ec8e0f90>
2002-08-14 18:16:47 +0000

    Improved the documentation appearance

--- trunk/website/htdocs/docs/async.php
+++ trunk/website/htdocs/docs/async.php
@@ -14,27 +14,27 @@
 <h2>SwingWorker: asynchronous solution</h2>
 
 <p>Solutions have been proposed for the <a href="freeze.php">GUI freeze problem</a>; asynchronous solutions
-rely on the combined usage of a worker thread and the SwingUtilities.invokeLater() method. We will see in few lines why
+rely on the combined usage of a worker thread and the <code>SwingUtilities.invokeLater()</code> method. We will see in few lines why
 they're called asynchronous.</p>
 <p>The main idea behind asynchronous solution is to return quickly from the time-consuming listener, after having
-delegated the time consuming task to a worker thread. The worker thread has to do 2 things:
+delegated the time-consuming task to a worker thread. The worker thread has to do 2 things:
 <ul>
 <li>Execute the time-consuming task
-<li>Post an event to the Event Queue using SwingUtilities.invokeLater()
+<li>Post an event to the Event Queue using <code>SwingUtilities.invokeLater()</code>
 </ul>
 </p>
 <p>Take a look at the code below which uses the <b>SwingWorker</b>, which is an asynchronous solution.</p>
-<p>Let's concentrate on the button's listener (the actionPerformed() method): the first statement, as in the freeze example, changes
+<p>Let's concentrate on the button's listener (the <code>actionPerformed()</code> method): the first statement, as in the freeze example, changes
 the text of the button and thus posts a repaint event to the Event Queue.<br>
 The next statement creates a SwingWorker object and starts it. This operation is quick, and non blocking.
-When a SwingWorker is started, a worker thread is also started for executing the code contained in construct();
-when the construct() method ends, the finished() method is called (using
-SwingUtilities.invokeLater()) and executed in the Event Dispatch Thread.<br>
+When a SwingWorker is started, a worker thread is also started for executing the code contained in <code>construct()</code>;
+when the <code>construct()</code> method ends, the <code>finished()</code> method is called (using
+<code>SwingUtilities.invokeLater()</code>) and executed in the Event Dispatch Thread.<br>
 So we create the SwingWorker, we start it, the listener finishes and returns; the Event Dispatch Thread
 can thus dequeue the next event and process it (very likely this event is the one posted by the first statement,
 that changes the button's text to "Sleeping...") .<br>
-When the worker thread finishes, the finished() method is posted as event in the Event Queue, and again the
-Event Dispatch Thread can dequeue it and process it, finally calling finished().<br>
+When the worker thread finishes, the <code>finished()</code> method is posted as event in the Event Queue, and again the
+Event Dispatch Thread can dequeue it and process it, finally calling <code>finished()</code>.<br>
 This is why these solutions are called asynchronous: they let the event listener return immediately, and the code the
 listener is supposed to execute is run asynchronously, while the listener still returns.
 </p>
@@ -43,34 +43,34 @@ listener is supposed to execute is run asynchronously, while the listener still 
 <li>Note the ugly exception handling. Even in this simple example, 3 chained if-else statements are required. Furthermore,
 the exception handling is done inside the SwingWorker, not inside the listener: there is no simple way to rethrow exceptions
 (it is possible but needs more coding).
-<li>Note the asymmetry: the first setText() is made outside the SwingWorker, the second inside of it
-<li>Note the confusing get() method: if construct() returns null (because the operation had a void return
-value - like Thread.sleep), it is easy to forget to call get() to see if any exception was thrown by the
+<li>Note the asymmetry: the first <code>setText()</code> is made outside the SwingWorker, the second inside of it
+<li>Note the confusing <code>get()</code> method: if <code>construct()</code> returns null (because the operation had a void return
+value - like <code>Thread.sleep()</code>), it is easy to forget to call <code>get()</code> to see if any exception was thrown by the
 time-consuming code.
-<li>What happens if the time-consuming code stays the same, but we want to execute 2 different finished() methods
+<li>What happens if the time-consuming code stays the same, but we want to execute 2 different <code>finished()</code> methods
 depending on the place from where we want to execute the time-consuming task ?
-<li>If some code is written after SwingWorker.start(), it will be always executed <em>before</em> finished().
+<li>If some code is written after <code>SwingWorker.start()</code>, it will be always executed <em>before</em> <code>finished()</code>.
 Thus looking at the code, we see:
 <ul>
-<li>setText("Sleeping...")
-<li>Thread.sleep()
-<li>setText("Slept !");
-<li>somethingElse()
+<li><code>setText("Sleeping...")</code>
+<li><code>Thread.sleep()</code>
+<li><code>setText("Slept !");</code>
+<li><code>somethingElse()</code>
 </ul>
 but the real order of execution is:
 <ul>
-<li>setText("Sleeping...")
-<li>somethingElse() [before, concurrently or after Thread.sleep()]
-<li>Thread.sleep()
-<li>setText("Slept !");
+<li><code>setText("Sleeping...")</code>
+<li><code>somethingElse()</code> [before, concurrently or after <code>Thread.sleep()</code>]
+<li><code>Thread.sleep()</code>
+<li><code>setText("Slept !");</code>
 </ul>
 making debugging and code readability very difficult.<br>
-This is why a golden rule of the SwingWorker is to never put code after SwingWorker.start().
-<li>If the code inside finished() requires a new time-consuming operation, a new <em>nested</em> SwingWorker should be used,
+This is why a golden rule of the SwingWorker is to never put code after <code>SwingWorker.start()</code>.
+<li>If the code inside <code>finished()</code> requires a new time-consuming operation, a new <em>nested</em> SwingWorker should be used,
 making the code complex and obscure, especially with respect to the sequence order of the operations executed.
 </ul>
 </p>
-<p>Fortunately, <a href="foxtrot.php">synchronous solution</a> solve these issues.</p>
+<p>Fortunately, <a href="foxtrot.php">Foxtrot</a> solves these issues.</p>
 <table width="100%" cellspacing="0" cellpadding="0">
 <tr><td width="60%">
 <pre><span class="code">
--- trunk/website/htdocs/docs/introduction.php
+++ trunk/website/htdocs/docs/introduction.php
@@ -20,8 +20,8 @@ Foundation Classes (JFC/Swing).</p>
 Foxtrot version 1.2 does not require to be in the boot classpath anymore (previous versions did), and it is thus compatible
 with the Java Web Start<sup><font="-2">TM</font></sup> technology.<br>
 <p>Below you can find an example of how to set the foxtrot core classes in the classpath.</p>
-<p>Let's suppose that your Swing application is contained in my-swing.jar, that the main class is called
-my.swing.Application, and the Foxtrot core classes in foxtrot.jar; then you should start your application
+<p>Let's suppose that your Swing application is contained in <code>my-swing.jar</code>, that the main class is called
+<code>my.swing.Application</code>, and the Foxtrot core classes in <code>foxtrot.jar</code>; then you should start your application
 with a command line similar to this one (under Windows):</p>
 <pre>
 > java -classpath foxtrot.jar;my-swing.jar my.swing.Application
@@ -30,8 +30,15 @@ with a command line similar to this one (under Windows):</p>
 <pre>
 > java -classpath foxtrot.jar:my-swing.jar my.swing.Application
 </pre>
-<p>The Foxtrot framework is released under the BSD license.</p>
-
+<p>The Foxtrot framework is released under the <a href="license.php">BSD license</a>.</p>
+<p>It is possible to use Foxtrot to develop and sell commercial Swing applications, provided that the requirements of the BSD
+license are met.<br/>
+These requirements aren't restrictive at all, and roughly say that when Swing applications developed using Foxtrot are
+redistributed (along with Foxtrot's binaries or source code), the Foxtrot license must be redistributed as well, for example
+in the documentation.<br/>
+A nice suggestion may be to use a Help/About dialog similar to Internet Explorer's, with a sentence like this:
+"This application has been developed using Foxtrot (http://foxtrot.sourceforge.net).
+Foxtrot is released under the BSD license, see the included documentation for further information".</p>
 </td></tr>
 
 <?php include 'footer.php';?>
