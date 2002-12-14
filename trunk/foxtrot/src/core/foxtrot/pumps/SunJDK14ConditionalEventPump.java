171f9e2aae64a276573b200e29b3b7c307e09e4e
biorn_steedom <biorn_steedom@d2b52d26-9d10-0410-9a7c-e9c4ec8e0f90>
2002-12-14 16:22:22 +0000

    Renamed from foxtrot.ConditionalEventPump to foxtrot.pumps.SunJDK14ConditionalEventPump

--- trunk/foxtrot/src/core/foxtrot/pumps/SunJDK14ConditionalEventPump.java
+++ trunk/foxtrot/src/core/foxtrot/pumps/SunJDK14ConditionalEventPump.java
@@ -6,28 +6,36 @@
  * See the terms of the BSD license in the documentation provided with this software.
  */
 
-package foxtrot;
+package foxtrot.pumps;
 
+import java.awt.AWTEvent;
+import java.awt.EventQueue;
+import java.awt.Toolkit;
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
-import java.lang.reflect.InvocationTargetException;
 import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.security.PrivilegedActionException;
 import java.security.PrivilegedExceptionAction;
 
-import javax.swing.SwingUtilities;
+import foxtrot.EventPump;
+import foxtrot.Task;
 
 /**
  * This implementation of EventPump calls the package protected method
- * <code>java.awt.EventDispatchThread.pumpEvents(Conditional)</code> and can be used with JDK 1.3+ only.
+ * <code>java.awt.EventDispatchThread.pumpEvents(Conditional)</code> and can be used with Sun JDK 1.4+ only.
  *
  * @author <a href="mailto:biorn_steedom@users.sourceforge.net">Simone Bordet</a>
  * @version $Revision$
  */
-public class ConditionalEventPump implements EventPump
+public class SunJDK14ConditionalEventPump implements EventPump
 {
+   private static final boolean debug = false;
+
    private static Class conditionalClass;
+   private static Class sequencedEventClass;
    private static Method pumpMethod;
 
    static
@@ -40,6 +48,7 @@ public class ConditionalEventPump implements EventPump
             {
                ClassLoader loader = ClassLoader.getSystemClassLoader();
                conditionalClass = loader.loadClass("java.awt.Conditional");
+               sequencedEventClass = loader.loadClass("java.awt.SequencedEvent");
                Class dispatchThreadClass = loader.loadClass("java.awt.EventDispatchThread");
                pumpMethod = dispatchThreadClass.getDeclaredMethod("pumpEvents", new Class[]{conditionalClass});
                pumpMethod.setAccessible(true);
@@ -50,13 +59,12 @@ public class ConditionalEventPump implements EventPump
                if (handler == null)
                {
                   handler = ThrowableHandler.class.getName();
-                  if (Worker.debug) System.out.println("Installing AWT Throwable Handler " + handler);
                   System.setProperty(property, handler);
-                  if (Worker.debug) System.out.println("Using newly installed AWT Throwable Handler " + handler);
+                  if (debug) System.out.println("[SunJDK14ConditionalEventPump] Installing AWT Throwable Handler " + handler);
                }
                else
                {
-                  if (Worker.debug) System.out.println("Using already installed AWT Throwable Handler " + handler);
+                  if (debug) System.out.println("[SunJDK14ConditionalEventPump] Using already installed AWT Throwable Handler " + handler);
                }
                return null;
             }
@@ -64,17 +72,17 @@ public class ConditionalEventPump implements EventPump
       }
       catch (PrivilegedActionException x)
       {
-         if (Worker.debug) x.printStackTrace();
+         if (debug) x.printStackTrace();
          throw new Error(x.toString());
       }
       catch (RuntimeException x)
       {
-         if (Worker.debug) x.printStackTrace();
+         if (debug) x.printStackTrace();
          throw x;
       }
       catch (Exception x)
       {
-         if (Worker.debug) x.printStackTrace();
+         if (debug) x.printStackTrace();
          throw new Error(x.toString());
       }
    }
@@ -96,18 +104,42 @@ public class ConditionalEventPump implements EventPump
       }
 
       /**
-       * Method <code>java.awt.Conditional.evaluate()</code> is implemented here to return
-       * true if the {@link Task} associated with this Conditional is NOT completed, and
-       * to return false if the associated task is completed.
+       * Implements method <code>java.awt.Conditional.evaluate()</code>
        */
       public Object invoke(Object proxy, Method method, Object[] args) throws Throwable
       {
          String name = method.getName();
-         if ("evaluate".equals(name))
+         if (!"evaluate".equals(name)) throw new Error("Unknown " + conditionalClass.getName() + " method: " + name);
+
+         // Now let's try to find a workaround for BUG #4531693 and related
+         AWTEvent nextEvent = getEventQueue().peekEvent();
+         if (debug) System.out.println("[SunJDK14ConditionalEventPump] Next Event: " + nextEvent);
+         if (sequencedEventClass.isInstance(nextEvent))
+         {
+            // Next event is a SequencedEvent; there are very good chances that the event from which Worker.post()
+            // is called (that ended up in calling this method) originated from another SequencedEvent.
+            // In this case, the SequencedEvent we will pump is not the first SequencedEvent, so this second
+            // SequencedEvent will wait for the first to be disposed (which cannot happen until we return
+            // from this method); this blocks the EventDispatchThread and hangs the Swing application.
+            // We return immediately, and will make the EventDispatchThread to wait until the Task is finished.
+            // Side effect is, of course, freezing of the GUI, which is however far better than a hang.
+            return Boolean.FALSE;
+         }
+         else
          {
             return task.isCompleted() ? Boolean.FALSE : Boolean.TRUE;
          }
-         throw new Error("Unknown " + conditionalClass.getName() + " method: " + name);
+      }
+
+      private EventQueue getEventQueue()
+      {
+         return (EventQueue)AccessController.doPrivileged(new PrivilegedAction()
+         {
+            public Object run()
+            {
+               return Toolkit.getDefaultToolkit().getSystemEventQueue();
+            }
+         });
       }
    }
 
@@ -117,6 +149,8 @@ public class ConditionalEventPump implements EventPump
     * and the AWT event dispatch mechanism calls it when an unexpected runtime exception or error
     * is thrown during event dispatching. If the user specifies a different exception handler,
     * this one will not be used, and the user's one is used instead.
+    * Use of this class is necessary in JDK 1.4, since RuntimeExceptions and Errors are propagated to
+    * be handled by the ThreadGroup (but not for modal dialogs).
     */
    public static class ThrowableHandler
    {
@@ -127,29 +161,21 @@ public class ConditionalEventPump implements EventPump
        */
       public void handle(Throwable t)
       {
-         System.err.println("Foxtrot - Exception occurred during event dispatching:");
+         System.err.println("[SunJDK14ConditionalEventPump] Exception occurred during event dispatching:");
          t.printStackTrace();
       }
    }
 
-   public ConditionalEventPump()
-   {
-   }
-
    public void pumpEvents(Task task)
    {
-      if (!SwingUtilities.isEventDispatchThread()) throw new IllegalStateException("This method can be called only from the AWT Event Dispatch Thread");
-
-      if (Worker.debug) System.out.println("EventPump " + this + " pumping events for task: " + task);
-
       // A null task may be passed for initialization of this class.
       if (task == null) return;
 
       try
       {
-         if (Worker.debug) System.out.println("EventPump " + this + " starts dequeueing events from standard AWT Event Queue for Task " + task);
+         if (debug) System.out.println("[SunJDK14ConditionalEventPump] Start pumping events - Pump is " + this + " - Task is " + task);
+
          // Invoke java.awt.EventDispatchThread.pumpEvents(new Conditional(task));
-         // which will block until the task is completed
          Object conditional = Proxy.newProxyInstance(conditionalClass.getClassLoader(), new Class[]{conditionalClass}, new Conditional(task));
          pumpMethod.invoke(Thread.currentThread(), new Object[]{conditional});
       }
@@ -158,34 +184,47 @@ public class ConditionalEventPump implements EventPump
          // No exceptions should escape from java.awt.EventDispatchThread.pumpEvents(Conditional)
          // since we installed a throwable handler. But one provided by the user may fail.
          Throwable t = x.getTargetException();
-         System.err.println("Foxtrot - Exception occurred during event dispatching:");
+         System.err.println("[SunJDK14ConditionalEventPump] Exception occurred during event dispatching:");
          t.printStackTrace();
 
-         System.err.println("Foxtrot - WARNING: uncaught exception during event dispatching, Task " + task + " may still be running !");
-
          // Rethrow. This will exit from Worker.post with a runtime exception or an error, and
-         // the original event pump will take care of it. Beware that the Task will continue to run
-         // It should never happen: the contract of the Worker.post method is strong: don't return
-         // until the Task has finished. We use awt exception handler to enforce this contract, but
-         // an awt exception handler provided by the user may fail to respect this contract.
+         // the original event pump will take care of it.
          if (t instanceof RuntimeException)
-         {
             throw (RuntimeException)t;
-         }
          else
-         {
             throw (Error)t;
-         }
       }
       catch (Throwable x)
       {
          // Here we have an implementation bug
-         System.err.println("Foxtrot - WARNING: uncaught exception in Foxtrot code, Task " + task + " is still running !");
+         System.err.println("[SunJDK14ConditionalEventPump] PANIC: uncaught exception in Foxtrot code");
          x.printStackTrace();
       }
       finally
       {
-         if (Worker.debug) System.out.println("Event pump " + this + " stops dequeueing events from standard AWT Event Queue for Task " + task);
+         // We're not done. Because of bug #4531693 (see Conditional) pumpEvents() may have returned immediately,
+         // but the Task is not completed. Same may happen in case of buggy exception handler.
+         // Here wait until the Task is completed. Side effect is freeze of GUI.
+         waitForTask(task);
+
+         if (debug) System.out.println("[SunJDK14ConditionalEventPump] Stop pumping events - Pump is " + this + " - Task is " + task);
+      }
+   }
+
+   private void waitForTask(Task task)
+   {
+      try
+      {
+         synchronized (task)
+         {
+            while (!task.isCompleted())
+            {
+               task.wait();
+            }
+         }
+      }
+      catch (InterruptedException ignored)
+      {
       }
    }
 }
