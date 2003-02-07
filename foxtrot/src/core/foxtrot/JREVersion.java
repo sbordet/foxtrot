e07b507fdb3d556e103d40c54363045cd3bde5fb
Simone Bordet <simone.bordet@gmail.com>
2003-02-07 16:45:34 +0000

    New methods to distinguish between JDK 1.4.0 and 1.4.1

--- foxtrot/src/core/foxtrot/JREVersion.java
+++ foxtrot/src/core/foxtrot/JREVersion.java
@@ -8,6 +8,9 @@
 
 package foxtrot;
 
+import java.security.AccessController;
+import java.security.PrivilegedAction;
+
 /**
  * Helper class that returns which is the current JRE version
  *
@@ -16,40 +19,75 @@ package foxtrot;
  */
 class JREVersion
 {
-   private static Boolean jre14;
+   private static Boolean jre141;
+   private static Boolean jre140;
    private static Boolean jre13;
    private static Boolean jre12;
 
-   static boolean isJRE14()
+   static boolean isJRE141()
    {
-      if (jre14 == null) jre14 = canLoadClass("java.nio.ByteBuffer");
-      return jre14.booleanValue();
+      if (jre141 == null)
+      {
+         Class cls = loadClass("java.awt.SequencedEvent");
+         if (cls == null) jre141 = Boolean.FALSE;
+         else jre141 = hasGetFirst(cls);
+      }
+      return jre141.booleanValue();
+   }
+
+   static boolean isJRE140()
+   {
+      if (jre140 == null)
+      {
+         Class cls = loadClass("java.awt.SequencedEvent");
+         if (cls == null) jre140 = Boolean.FALSE;
+         else jre140 = hasGetFirst(cls).booleanValue() ? Boolean.FALSE : Boolean.TRUE;
+      }
+      return jre140.booleanValue();
    }
 
    static boolean isJRE13()
    {
-      if (jre13 == null) jre13 = canLoadClass("java.lang.reflect.Proxy");
+      if (jre13 == null) jre13 = loadClass("java.lang.reflect.Proxy") == null ? Boolean.FALSE : Boolean.TRUE;
       return jre13.booleanValue();
    }
 
    static boolean isJRE12()
    {
-      if (jre12 == null) jre12 = canLoadClass("java.util.Collection");
+      if (jre12 == null) jre12 = loadClass("java.util.Collection") == null ? Boolean.FALSE : Boolean.TRUE;
       return jre12.booleanValue();
    }
 
-   private static Boolean canLoadClass(String className)
+   private static Class loadClass(String className)
    {
       // Avoid some smart guy puts the classes in the classpath or in lib/ext.
       // We ask directly to the boot classloader
       try
       {
-         Class.forName(className, false, null);
-         return Boolean.TRUE;
+         return Class.forName(className, false, null);
       }
       catch (ClassNotFoundException ignored)
       {
       }
-      return Boolean.FALSE;
+      return null;
+   }
+
+   private static Boolean hasGetFirst(final Class cls)
+   {
+      return (Boolean)AccessController.doPrivileged(new PrivilegedAction()
+      {
+         public Object run()
+         {
+            try
+            {
+               cls.getDeclaredMethod("getFirst", null);
+               return Boolean.TRUE;
+            }
+            catch (Exception ignored)
+            {
+            }
+            return Boolean.FALSE;
+         }
+      });
    }
 }
