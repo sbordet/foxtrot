6c55fb13c115a30d9735cb83879875c398351746
biorn_steedom <biorn_steedom@d2b52d26-9d10-0410-9a7c-e9c4ec8e0f90>
2003-07-03 08:12:35 +0000

    Using invokeTest() to call the tests

--- trunk/foxtrot/src/test/foxtrot/test/SingleWorkerThreadTest.java
+++ trunk/foxtrot/src/test/foxtrot/test/SingleWorkerThreadTest.java
@@ -24,297 +24,357 @@ public class SingleWorkerThreadTest extends FoxtrotTestCase
 
    public void testStart() throws Exception
    {
-      TestSingleWorkerThread worker = new TestSingleWorkerThread();
-      if (worker.isAlive()) fail();
-      worker.start();
-      if (!worker.isAlive()) fail();
-      // This sleep will ensure that the JUnit JVM does not exit before the WorkerThread has done its job
-      Thread.sleep(500);
+      invokeTest(new Runnable()
+      {
+         public void run()
+         {
+            TestSingleWorkerThread worker = new TestSingleWorkerThread();
+            if (worker.isAlive()) fail();
+            worker.start();
+            if (!worker.isAlive()) fail();
+         }
+      });
    }
 
    public void testStop() throws Exception
    {
-      TestSingleWorkerThread worker = new TestSingleWorkerThread();
-      worker.start();
-      worker.stop();
-      if (worker.isAlive()) fail();
-      Thread.sleep(500);
+      invokeTest(new Runnable()
+      {
+         public void run()
+         {
+            TestSingleWorkerThread worker = new TestSingleWorkerThread();
+            worker.start();
+            worker.stop();
+            if (worker.isAlive()) fail();
+         }
+      });
    }
 
    public void testStartStopStart() throws Exception
    {
-      TestSingleWorkerThread worker = new TestSingleWorkerThread();
-      worker.start();
-      worker.stop();
-      worker.start();
-      if (!worker.isAlive()) fail();
-      Thread.sleep(500);
+      invokeTest(new Runnable()
+      {
+         public void run()
+         {
+            TestSingleWorkerThread worker = new TestSingleWorkerThread();
+            worker.start();
+            worker.stop();
+            worker.start();
+            if (!worker.isAlive()) fail();
+         }
+      });
    }
 
    public void testStopBeforeStart() throws Exception
    {
-      TestSingleWorkerThread worker = new TestSingleWorkerThread();
-      worker.stop();
-      Thread.sleep(500);
+      invokeTest(new Runnable()
+      {
+         public void run()
+         {
+            TestSingleWorkerThread worker = new TestSingleWorkerThread();
+            worker.stop();
+         }
+      });
    }
 
    public void testStartStart() throws Exception
    {
-      final MutableHolder thread = new MutableHolder(null);
-      TestSingleWorkerThread worker = new TestSingleWorkerThread()
+      invokeTest(new Runnable()
       {
          public void run()
          {
-            thread.set(Thread.currentThread());
-            super.run();
+            final MutableHolder thread = new MutableHolder(null);
+            TestSingleWorkerThread worker = new TestSingleWorkerThread()
+            {
+               public void run()
+               {
+                  thread.set(Thread.currentThread());
+                  super.run();
+               }
+            };
+            worker.start();
+            sleep(500);
+            Thread foxtrot = (Thread)thread.get();
+            worker.start();
+            sleep(500);
+            if (foxtrot != thread.get()) fail();
          }
-      };
-      worker.start();
-      Thread.sleep(500);
-      Thread foxtrot = (Thread)thread.get();
-      worker.start();
-      Thread.sleep(500);
-      if (foxtrot != thread.get()) fail();
-      Thread.sleep(500);
+      });
    }
 
    public void testNoStartAllowsPost() throws Exception
    {
-      TestSingleWorkerThread worker = new TestSingleWorkerThread();
-      final MutableInteger pass = new MutableInteger(0);
-      worker.postTask(new Task()
+      invokeTest(new Runnable()
       {
-         public Object run() throws Exception
+         public void run()
          {
-            pass.set(1);
-            return null;
+            TestSingleWorkerThread worker = new TestSingleWorkerThread();
+            final MutableInteger pass = new MutableInteger(0);
+            worker.postTask(new Task()
+            {
+               public Object run() throws Exception
+               {
+                  pass.set(1);
+                  return null;
+               }
+            });
+            sleep(500);
+            if (pass.get() != 1) fail();
          }
       });
-      Thread.sleep(500);
-      if (pass.get() != 1) fail();
    }
 
    public void testPost() throws Exception
    {
-      TestSingleWorkerThread worker = new TestSingleWorkerThread();
-      worker.start();
-      final MutableInteger pass = new MutableInteger(0);
-      worker.postTask(new Task()
+      invokeTest(new Runnable()
       {
-         public Object run() throws Exception
+         public void run()
          {
-            pass.set(1);
-            return null;
+            TestSingleWorkerThread worker = new TestSingleWorkerThread();
+            worker.start();
+            final MutableInteger pass = new MutableInteger(0);
+            worker.postTask(new Task()
+            {
+               public Object run() throws Exception
+               {
+                  pass.set(1);
+                  return null;
+               }
+            });
+            sleep(500);
+            if (pass.get() != 1) fail();
          }
       });
-      Thread.sleep(100);
-      if (pass.get() != 1) fail();
-      Thread.sleep(500);
    }
 
    public void testManyPosts() throws Exception
    {
-      TestSingleWorkerThread worker = new TestSingleWorkerThread();
-      worker.start();
-      worker.postTask(new Task()
+      invokeTest(new Runnable()
       {
-         public Object run() throws Exception
+         public void run()
          {
-            Thread.sleep(500);
-            return null;
+            TestSingleWorkerThread worker = new TestSingleWorkerThread();
+            worker.start();
+            worker.postTask(new Task()
+            {
+               public Object run() throws Exception
+               {
+                  sleep(500);
+                  return null;
+               }
+            });
+            final MutableInteger pass = new MutableInteger(0);
+            worker.postTask(new Task()
+            {
+               public Object run() throws Exception
+               {
+                  pass.set(pass.get() + 1);
+                  return null;
+               }
+            });
+            worker.postTask(new Task()
+            {
+               public Object run() throws Exception
+               {
+                  pass.set(pass.get() + 1);
+                  return null;
+               }
+            });
+            sleep(1000);
+            if (pass.get() != 2) fail();
          }
       });
-      final MutableInteger pass = new MutableInteger(0);
-      worker.postTask(new Task()
-      {
-         public Object run() throws Exception
-         {
-            pass.set(pass.get() + 1);
-            return null;
-         }
-      });
-      worker.postTask(new Task()
-      {
-         public Object run() throws Exception
-         {
-            pass.set(pass.get() + 1);
-            return null;
-         }
-      });
-      Thread.sleep(1000);
-      if (pass.get() != 2) fail();
-      Thread.sleep(500);
    }
 
    public void testTaskIsExecutedAfterIgnoredInterruptInTask() throws Exception
    {
-      TestSingleWorkerThread worker = new TestSingleWorkerThread();
-      final MutableHolder thread = new MutableHolder(null);
-      final MutableInteger pass = new MutableInteger(0);
-      worker.postTask(new Task()
+      invokeTest(new Runnable()
       {
-         public Object run() throws Exception
+         public void run()
          {
-            thread.set(Thread.currentThread());
-
-            try
+            TestSingleWorkerThread worker = new TestSingleWorkerThread();
+            final MutableHolder thread = new MutableHolder(null);
+            final MutableInteger pass = new MutableInteger(0);
+            worker.postTask(new Task()
             {
-               Thread.sleep(1000);
-            }
-            catch (InterruptedException x)
-            {
-               // Swallow the exception and don't restore the interrupted status of the thread
-               // Be sure we pass here
-               pass.set(1);
-            }
+               public Object run() throws Exception
+               {
+                  thread.set(Thread.currentThread());
 
-            return null;
+                  try
+                  {
+                     Thread.sleep(1000);
+                  }
+                  catch (InterruptedException x)
+                  {
+                     // Swallow the exception and don't restore the interrupted status of the thread
+                     // Be sure we pass here
+                     pass.set(1);
+                  }
+
+                  return null;
+               }
+            });
+            sleep(500);
+
+            // Interrupt the WorkerThread.
+            // The task will not restore the status of the thread, thereby allowing it to continue
+            Thread foxtrot = (Thread)thread.get();
+            foxtrot.interrupt();
+            sleep(1000);
+            if (pass.get() != 1) fail();
+
+            // Be sure another Task can be posted and executed on the same worker thread as above
+            worker.postTask(new Task()
+            {
+               public Object run() throws Exception
+               {
+                  thread.set(Thread.currentThread());
+                  pass.set(2);
+                  return null;
+               }
+            });
+            sleep(500);
+            if (thread.get() != foxtrot) fail();
+            if (pass.get() != 2) fail();
          }
       });
-      Thread.sleep(500);
-
-      // Interrupt the WorkerThread.
-      // The task will not restore the status of the thread, thereby allowing it to continue
-      Thread foxtrot = (Thread)thread.get();
-      foxtrot.interrupt();
-      Thread.sleep(1000);
-      if (pass.get() != 1) fail();
-
-      // Be sure another Task can be posted and executed on the same worker thread as above
-      worker.postTask(new Task()
-      {
-         public Object run() throws Exception
-         {
-            thread.set(Thread.currentThread());
-            pass.set(2);
-            return null;
-         }
-      });
-      Thread.sleep(500);
-      if (thread.get() != foxtrot) fail();
-      if (pass.get() != 2) fail();
-      Thread.sleep(500);
    }
 
    public void testTaskIsExecutedAfterNotIgnoredInterruptInTask() throws Exception
    {
-      TestSingleWorkerThread worker = new TestSingleWorkerThread();
-      final MutableHolder thread = new MutableHolder(null);
-      final MutableInteger pass = new MutableInteger(0);
-      worker.postTask(new Task()
+      invokeTest(new Runnable()
       {
-         public Object run() throws Exception
+         public void run()
          {
-            thread.set(Thread.currentThread());
-
-            try
+            TestSingleWorkerThread worker = new TestSingleWorkerThread();
+            final MutableHolder thread = new MutableHolder(null);
+            final MutableInteger pass = new MutableInteger(0);
+            worker.postTask(new Task()
             {
-               Thread.sleep(1000);
-            }
-            catch (InterruptedException x)
-            {
-               // Restore the interrupted status of the thread
-               Thread.currentThread().interrupt();
-               // Be sure we pass here
-               pass.set(1);
-            }
+               public Object run() throws Exception
+               {
+                  thread.set(Thread.currentThread());
 
-            return null;
+                  try
+                  {
+                     Thread.sleep(1000);
+                  }
+                  catch (InterruptedException x)
+                  {
+                     // Restore the interrupted status of the thread
+                     Thread.currentThread().interrupt();
+                     // Be sure we pass here
+                     pass.set(1);
+                  }
+
+                  return null;
+               }
+            });
+            sleep(500);
+
+            // Interrupt the WorkerThread.
+            // The task will not restore the status of the thread, thereby allowing it to continue
+            Thread foxtrot = (Thread)thread.get();
+            foxtrot.interrupt();
+            sleep(1000);
+            if (pass.get() != 1) fail();
+
+            // Be sure another Task can be posted and executed on a different worker thread
+            worker.postTask(new Task()
+            {
+               public Object run() throws Exception
+               {
+                  thread.set(Thread.currentThread());
+                  pass.set(2);
+                  return null;
+               }
+            });
+            sleep(500);
+            if (thread.get() == foxtrot) fail();
+            if (pass.get() != 2) fail();
          }
       });
-      Thread.sleep(500);
-
-      // Interrupt the WorkerThread.
-      // The task will not restore the status of the thread, thereby allowing it to continue
-      Thread foxtrot = (Thread)thread.get();
-      foxtrot.interrupt();
-      Thread.sleep(1000);
-      if (pass.get() != 1) fail();
-
-      // Be sure another Task can be posted and executed on a different worker thread
-      worker.postTask(new Task()
-      {
-         public Object run() throws Exception
-         {
-            thread.set(Thread.currentThread());
-            pass.set(2);
-            return null;
-         }
-      });
-      Thread.sleep(500);
-      if (thread.get() == foxtrot) fail();
-      if (pass.get() != 2) fail();
-      Thread.sleep(500);
    }
 
    public void testTaskIsExecutedAfterInterruptInTask() throws Exception
    {
-      TestSingleWorkerThread worker = new TestSingleWorkerThread();
-      final MutableHolder thread = new MutableHolder(null);
-      final MutableInteger pass = new MutableInteger(0);
-      worker.postTask(new Task()
+      invokeTest(new Runnable()
       {
-         public Object run() throws Exception
+         public void run()
          {
-            thread.set(Thread.currentThread());
+            TestSingleWorkerThread worker = new TestSingleWorkerThread();
+            final MutableHolder thread = new MutableHolder(null);
+            final MutableInteger pass = new MutableInteger(0);
+            worker.postTask(new Task()
+            {
+               public Object run() throws Exception
+               {
+                  thread.set(Thread.currentThread());
 
-            // Allow InterruptedException to unwind the stack frames
-            Thread.sleep(1000);
+                  // Allow InterruptedException to unwind the stack frames
+                  Thread.sleep(1000);
 
-            return null;
+                  return null;
+               }
+            });
+            sleep(500);
+
+            Thread foxtrot = (Thread)thread.get();
+            foxtrot.interrupt();
+            sleep(1000);
+
+            // Be sure another Task can be posted and executed on a different worker thread
+            worker.postTask(new Task()
+            {
+               public Object run() throws Exception
+               {
+                  thread.set(Thread.currentThread());
+                  pass.set(1);
+                  return null;
+               }
+            });
+            sleep(500);
+            if (thread.get() == foxtrot) fail();
+            if (pass.get() != 1) fail();
          }
       });
-      Thread.sleep(500);
-
-      Thread foxtrot = (Thread)thread.get();
-      foxtrot.interrupt();
-      Thread.sleep(1000);
-
-      // Be sure another Task can be posted and executed on a different worker thread
-      worker.postTask(new Task()
-      {
-         public Object run() throws Exception
-         {
-            thread.set(Thread.currentThread());
-            pass.set(1);
-            return null;
-         }
-      });
-      Thread.sleep(500);
-      if (thread.get() == foxtrot) fail();
-      if (pass.get() != 1) fail();
-      Thread.sleep(500);
    }
 
    public void testPendingTasksAreExecutedAfterRestart() throws Exception
    {
-      TestSingleWorkerThread worker = new TestSingleWorkerThread();
-      worker.start();
-      worker.postTask(new Task()
+      invokeTest(new Runnable()
       {
-         public Object run() throws Exception
+         public void run()
          {
-            Thread.sleep(500);
-            return null;
+            TestSingleWorkerThread worker = new TestSingleWorkerThread();
+            worker.start();
+            worker.postTask(new Task()
+            {
+               public Object run() throws Exception
+               {
+                  Thread.sleep(500);
+                  return null;
+               }
+            });
+            final MutableInteger pass = new MutableInteger(0);
+            worker.postTask(new Task()
+            {
+               public Object run() throws Exception
+               {
+                  pass.set(1);
+                  return null;
+               }
+            });
+            sleep(250);
+            worker.stop();
+            sleep(1000);
+            // Be sure 2nd Task not yet executed
+            if (pass.get() != 0) fail();
+            worker.start();
+            sleep(500);
+            if (pass.get() != 1) fail();
          }
       });
-      final MutableInteger pass = new MutableInteger(0);
-      worker.postTask(new Task()
-      {
-         public Object run() throws Exception
-         {
-            pass.set(1);
-            return null;
-         }
-      });
-      Thread.sleep(250);
-      worker.stop();
-      Thread.sleep(1000);
-      // Be sure 2nd Task not yet executed
-      if (pass.get() != 0) fail();
-      worker.start();
-      Thread.sleep(500);
-      if (pass.get() != 1) fail();
-      Thread.sleep(500);
    }
 }
