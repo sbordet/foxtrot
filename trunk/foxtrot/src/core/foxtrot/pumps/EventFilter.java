5857abef7b803c06ca34467cbdce324ae63f0ee6
biorn_steedom <biorn_steedom@d2b52d26-9d10-0410-9a7c-e9c4ec8e0f90>
2004-03-27 11:27:17 +0000

    Updated Javadocs to reflect new behavior: with JDK 1.4 event filtering, it is not true anymore that the filtered event has already been removed from the queue when the filter is called

--- trunk/foxtrot/src/core/foxtrot/pumps/EventFilter.java
+++ trunk/foxtrot/src/core/foxtrot/pumps/EventFilter.java
@@ -12,7 +12,6 @@ import java.awt.AWTEvent;
 
 /**
  * Filters AWT events pumped by {@link foxtrot.EventPump EventPump}s before they're dispatched.
- *
  * @see EventFilterable
  * @author <a href="mailto:biorn_steedom@users.sourceforge.net">Simone Bordet</a>
  * @version $Revision$
@@ -20,10 +19,7 @@ import java.awt.AWTEvent;
 public interface EventFilter
 {
    /**
-    * Callback called by {@link foxtrot.EventPump EventPump}s to filter the given AWT event. <br>
-    * This method is called before the event is actually dispatched
-    * but after the event has been removed from the EventQueue.
-    *
+    * Callback called by {@link foxtrot.EventPump EventPump}s to filter the given AWT event.
     * @param event The event to filter
     * @return True if the event should be dispatched, false otherwise
     */
