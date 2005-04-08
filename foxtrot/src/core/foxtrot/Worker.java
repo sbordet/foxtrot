6e6d0eaf4e6cfe70b16ff321da514164d6ac9411
Simone Bordet <simone.bordet@gmail.com>
2005-04-08 21:14:44 +0000

    Updated javadocs

--- foxtrot/src/core/foxtrot/Worker.java
+++ foxtrot/src/core/foxtrot/Worker.java
@@ -8,14 +8,12 @@
 
 package foxtrot;
 
-import javax.swing.SwingUtilities;
-
 import foxtrot.workers.SingleWorkerThread;
 
 /**
- * The class that execute time-consuming {@link Task}s and {@link Job}s. <br>
+ * The class that execute time-consuming {@link Task}s and {@link Job}s. <br />
  * It is normally used in event listeners that must execute time-consuming operations without
- * freezing the Swing GUI. <br>
+ * freezing the Swing GUI. <br />
  * Usage example (simplified from the Foxtrot examples):
  * <pre>
  * JButton button = new JButton("Take a nap!");
@@ -40,10 +38,11 @@ import foxtrot.workers.SingleWorkerThread;
  * </pre>
  *
  * While normally not necessary, it is possible to customize the two core components of this
- * class, the {@link EventPump} and the {@link WorkerThread}, explicitely via API or by
- * setting the system properties <code>foxtrot.event.pump</code> and
- * <code>foxtrot.worker.thread</code>, respectively, to a full qualified name of a class
- * implementing, respectively, the above interfaces.
+ * class, the {@link EventPump} and the {@link WorkerThread} via the corrispondent setter 
+ * methods. <br />
+ * This class uses by default a WorkerThread that enqueues Tasks; Tasks will be executed one
+ * after the other. This is the behavior needed by most applications that want to rely on a 
+ * synchronous model. For an alternative behavior, see {@link ConcurrentWorker}
  *
  * @version $Revision$
  */
@@ -61,6 +60,8 @@ public class Worker extends AbstractSyncWorker
    /**
     * Returns the WorkerThread used to run {@link foxtrot.Task}s subclasses in a thread
     * that is not the Event Dispatch Thread.
+    * By default, the WorkerThread will enqueue the Tasks and execute them one after the
+    * other.
     * @see #setWorkerThread
     */
    public static WorkerThread getWorkerThread()
@@ -85,11 +86,10 @@ public class Worker extends AbstractSyncWorker
    }
 
    /**
-    * Returns the EventPump used to pump events from the AWT Event Queue.
+    * Returns the EventPump used to pump events from the AWT Event Queue. <br />
     * If no calls to {@link #setEventPump} have been made a default pump is returned;
-    * this default pump is first obtained by looking up a class name specified by the
-    * system property <code>foxtrot.event.pump</code>, then by instantiating the
-    * suitable pump for the Java version that is running the code.
+    * this default pump is obtained by instantiating the suitable pump for the 
+    * Java version that is running the code.
     * @see #setEventPump
     */
    public static EventPump getEventPump()
@@ -112,15 +112,17 @@ public class Worker extends AbstractSyncWorker
    }
 
    /**
-    * Enqueues the given Task to be executed in the worker thread. <br>
-    * If this method is called from the Event Dispatch Thread, it blocks until the task has been executed,
-    * either by finishing normally or throwing an exception. <br>
-    * If this method is called from another Task,
-    * it executes the new Task immediately and then returns the control to the calling Task. <br>
-    * While executing Tasks, it dequeues AWT events from the AWT Event Queue; even in case of AWT events
-    * that throw RuntimeExceptions or Errors, this method will not return until the first Task
-    * (posted from the Event Dispatch Thread) is finished.
-    * @throws IllegalStateException if is not called from the Event Dispatch Thread nor from another Task.
+    * Enqueues the given Task to be executed by the WorkerThread, while dequeueing
+    * AWT events. <br />
+    * If this method is called from the Event Dispatch Thread, it blocks until the task 
+    * has been executed, either by finishing normally or throwing an exception. <br />
+    * If this method is called from a worker thread, it executes the new Task 
+    * immediately in the same thread and then returns the control to the calling Task. <br />
+    * While Tasks are executed, AWT events are dequeued from the AWT Event Queue; 
+    * even in case of AWT events that throw RuntimeExceptions or Errors, this method will 
+    * not return until the first Task (posted from the Event Dispatch Thread) is finished.
+    * @throws IllegalStateException if is not called from the Event Dispatch Thread nor
+    * from a worker thread.
     * @see #post(Job job)
     */
    public static Object post(Task task) throws Exception
@@ -129,7 +131,7 @@ public class Worker extends AbstractSyncWorker
    }
 
    /**
-    * Enqueues the given Job to be executed in the worker thread. <br>
+    * Enqueues the given Job to be executed in the worker thread. <br />
     * This method behaves exactly like {@link #post(Task task)}, but it does not throw checked exceptions.
     * @throws IllegalStateException if is not called from the Event Dispatch Thread nor from another Job or Task.
     * @see #post(Task)
