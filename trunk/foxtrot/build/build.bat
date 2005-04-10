a676bce3ac4b0225edfab0a4968d5b283681440c
simonebordet <simonebordet@d2b52d26-9d10-0410-9a7c-e9c4ec8e0f90>
2005-04-10 21:07:53 +0000

    + Ported the build system to Ant 1.6.2 + Generating MANIFEST.MF from build file + Build can be done only with JDK 1.4+, but compilation targets 1.1

--- trunk/foxtrot/build/build.bat
+++ trunk/foxtrot/build/build.bat
@@ -3,16 +3,14 @@
 setlocal
 
 if not "%JAVA_HOME%"=="" goto start
-
 echo JAVA_HOME not set
 goto end
 
 :start
 
 set CP=ant.jar
-set CP=%CP%;optional.jar
-set CP=%CP%;jaxp.jar
-set CP=%CP%;crimson.jar
+set CP=%CP%;ant-launcher.jar
+set CP=%CP%;ant-junit.jar
 set CP=%CP%;..\lib\junit.jar
 set CP=%CP%;%JAVA_HOME%\lib\tools.jar
 
