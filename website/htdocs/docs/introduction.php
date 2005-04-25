5e1feed01f5cff73d1b6bb4d415d85dcfce59b00
Simone Bordet <simone.bordet@gmail.com>
2005-04-25 17:21:09 +0000

    no message

--- website/htdocs/docs/introduction.php
+++ website/htdocs/docs/introduction.php
@@ -1,5 +1,5 @@
 <?php include 'header.php';?>
-
+<!--
 <tr><td align="center" colspan="3">
 <a href="http://foxtrot.sourceforge.net"><img class="nav-btn-en" hspace="10" src="../images/cnvhome.gif"/></a>
 <a href="toc.php"><img class="nav-btn-en" hspace="10" src="../images/cnvup.gif"/></a>
@@ -8,14 +8,14 @@
 </td></tr>
 
 <tr><td class="date" colspan="3">Last Updated: $Date$</td></tr>
-
+-->
 <tr><td class="documentation">
 
 <h2>Introduction</h2>
 
 <p>Foxtrot is a small but powerful framework for using threads with the Java<sup><font="-2">TM</font></sup>
 Foundation Classes (JFC/Swing).</p>
-<p>It is compatible with the J2SE<sup><font="-2">TM</font></sup>, and has been tested with version 1.3.x and 1.4.x.</p>
+<p>It is compatible with the J2SE<sup><font="-2">TM</font></sup>, and has been tested with version 1.3.x, 1.4.x and 5.0.x</p>
 <p>The jar containing the Foxtrot core classes is called foxtrot.jar, and should be included in the classpath. <br>
 Foxtrot version 1.2 does not require to be in the boot classpath anymore (previous versions did), and it is thus compatible
 with the Java Web Start<sup><font="-2">TM</font></sup> technology.<br>
