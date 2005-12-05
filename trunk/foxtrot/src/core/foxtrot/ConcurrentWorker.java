72ae6fa4bace33c24cc0b24a010de7c664233a72
simonebordet <simonebordet@d2b52d26-9d10-0410-9a7c-e9c4ec8e0f90>
2005-12-05 04:31:36 +0000

    Javadocs

--- trunk/foxtrot/src/core/foxtrot/ConcurrentWorker.java
+++ trunk/foxtrot/src/core/foxtrot/ConcurrentWorker.java
@@ -12,19 +12,38 @@ import foxtrot.workers.MultiWorkerThread;
 
 /**
  * The class that execute time-consuming {@link Task}s and {@link Job}s, but differently
- * from {@link Worker}, execute them concurrently and not one after the other. <br />
+ * from {@link Worker}, execute them concurrently and not one after the other, though
+ * the invocations to {@link #post(Task)} or {@link #post(Job)} will finish one after
+ * the other. <br />
+ * <br />
  * Most of the times, the best choice for having synchronous behavior is {@link Worker},
  * so please think twice before using this class. <br />
- * A typical usage of this class is, for example, in an application with multiple tabs
- * where the user triggers the loading of the content of the tab by clicking on it,
- * and where tab loading is time-consuming; in such scenario, the behavior of
- * {@link Worker} would be to load the first tab and only when the first tab is loaded
- * start loading the second tab.
- * The behavior of ConcurrentWorker would be to load the tabs concurrently. <br />
+ * <br />
+ * A typical usage of this class is, for example, in an application that needs to
+ * communicate to a server to submit tasks and to cancel them. <br />
+ * Submitting a task to the server requires an event from the user, <i>event1</i> - for
+ * example clicking on a submit button, which will call <code>ConcurrentWorker.post()</code>
+ * to submit the task <i>task1</i> to the server in <i>thread1</i>.
+ * If then the user wants to cancel <i>task1</i>, it generates a new event, <i>event2</i> -
+ * for example clicking on a cancel button, which will call <code>ConcurrentWorker.post()</code>
+ * to submit a task <i>task2</i> to the server in <i>thread2</i> that will cancel
+ * <i>task1</i>. <br />
+ * However, since <i>event2</i> is dequeued by Foxtrot during the execution of <i>task1</i>,
+ * the first invocation of <code>ConcurrentWorker.post()</code> (the one that submitted the
+ * task to the server) does not return until <i>event2</i> and hence the second invocation
+ * of <code>ConcurrentWorker.post()</code> is finished. <br />
+ * Therefore, this class will execute tasks concurrently, but invocations of
+ * <code>ConcurrentWorker.post()</code> are executed one after the other.
+ * This ensures that the code after the second invocation of
+ * <code>ConcurrentWorker.post()</code> is always executed before the code after the first
+ * invocation of <code>ConcurrentWorker.post()</code> (this is not guaranteed when
+ * using {@link AsyncWorker}.
+ * <br />
+ * Using {@link Worker} would make it impossible to cancel the task, since <i>task2</i> will
+ * be executed only when <i>task1</i> is finished. <br />
+ * <br />
  * Needless to say, this concurrent behavior imposes further care in designing the
- * system to respect thread safety, and to keep the system simple to understand. <br />
- * This approach is very similar to the asynchronous behavior, but with the advantages
- * of the synchronous model in code readability.
+ * system to respect thread safety, and to keep the system simple to understand.
  */
 public class ConcurrentWorker extends AbstractSyncWorker
 {
