f63cc29cd8e2f722bfb6303de31b8be556520052
Simone Bordet <simone.bordet@gmail.com>
2006-07-03 15:43:33 +0000

    Added build target to copy website files to SourceForge.

--- foxtrot/build/build.bat
+++ foxtrot/build/build.bat
@@ -11,7 +11,10 @@ goto end
 set CP=ant.jar
 set CP=%CP%;ant-launcher.jar
 set CP=%CP%;ant-junit.jar
+set CP=%CP%;ant-jsch.jar
+set CP=%CP%;..\lib\maven-artifact-ant-2.0.4-dep.jar
 set CP=%CP%;..\lib\junit.jar
+set CP=%CP%;..\lib\jsch-0.1.28.jar
 set CP=%CP%;%JAVA_HOME%\lib\tools.jar
 
 %JAVA_HOME%\bin\java -cp "%CP%" org.apache.tools.ant.Main %*
