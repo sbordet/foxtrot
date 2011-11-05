bd22c63695c4d15e5c781ad9f140ba0648b9a329
simonebordet <simonebordet@d2b52d26-9d10-0410-9a7c-e9c4ec8e0f90>
2011-11-05 16:41:42 +0000

    Rolled back failed release.

--- tags/foxtrot-4.0/tags/Foxtrot_3_0/upload-website.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-
-rsync --recursive --progress --verbose --exclude=".svn" --rsh="ssh -l simonebordet" website/htdocs/* simonebordet@shell.sourceforge.net:/home/groups/f/fo/foxtrot/htdocs
